b7f8cbe28973f2ddea903c1eee9205ae866edc40
Neeraj VK <neeraj@karkhana.io>
2022-04-07 19:55:46 +0000

    added option for enable/disable sync

--- indiamart_integration/api.py
+++ indiamart_integration/api.py
@@ -51,6 +51,9 @@ def get_request_url(india_mart_setting):
 
 @frappe.whitelist()
 def cron_sync_lead():
+	india_mart_setting = frappe.get_doc("IndiaMart Setting","IndiaMart Setting")
+	if not india_mart_setting.enabled:
+		return
 	try:
 		sync_india_mart_lead(today(),today())
 	except Exception as e:
@@ -58,16 +61,32 @@ def cron_sync_lead():
 
 @frappe.whitelist()
 def add_lead(lead_data):
+	qtype_map = {'P' : 'Indiamart – Call', 'B' : 'Indiamart – Buy Lead', 'W' : 'Indiamart – Direct'}
 	try:
 		if not frappe.db.exists("Lead",{"india_mart_id":lead_data["QUERY_ID"]}):
 			doc = frappe.get_doc(dict(
+				# doctype="Lead",
+				# title = lead_data['GLUSR_USR_COMPANYNAME'] if lead_data['GLUSR_USR_COMPANYNAME'] else lead_data['SENDERNAME'],
+				# lead_name = lead_data["SENDERNAME"],
+				# email_id = lead_data["SENDEREMAIL"],
+				# mobile_no = lead_data["MOB"][-10:],
+				# company_name = lead_data['GLUSR_USR_COMPANYNAME'],
+				# address_line1 = lead_data['ENQ_ADDRESS'],
+				# city = lead_data['ENQ_CITY'],
+				# state = lead_data['ENQ_STATE'],
+				# notes = lead_data['ENQ_MESSAGE'] + '\n' + lead_data['PRODUCT_NAME'] + '\n' + lead_data['ENQ_CALL_DURATION'] + '\n' + lead_data['ENQ_RECEIVER_MOB'] + '\n' + lead_data['EMAIL_ALT'] + '\n' + lead_data['QUERY_ID'],
+				# phone = lead_data['MOBILE_ALT'],
+				# status = 'Lead',
+				# source = qtype_map[lead_data["QTYPE"]]
+				
 				doctype="Lead",
 				lead_name=lead_data["SENDERNAME"],
 				email_address=lead_data["SENDEREMAIL"],
 				phone=lead_data["MOB"][-10:],
 				requirement=lead_data["SUBJECT"],
 				india_mart_id=lead_data["QUERY_ID"],
-				source="India Mart"           
+				source="India Mart" 
+				          
 			)).insert(ignore_permissions = True)
 			return doc
 	except Exception as e:
